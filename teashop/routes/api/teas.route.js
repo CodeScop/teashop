df26c94f50b239da5543b84b57fafadc5155caa2
Stefanie Ward <StefBWard@gmail.com>
2018-12-28 14:45:43 -0500

    recreate repo for full app

---
+++ teashop/routes/api/teas.route.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+var TeaController = require('../../controllers/tea.controller.js');
+
+router.get('/', TeaController.getTeas);
+router.post('/', TeaController.createTea);
+router.put('/', TeaController.updateTea);
+router.delete('/:id', TeaController.removeTea);
+
+module.exports = router;

538838fe45746749d144add482eb24c5e7ece09b
Andrew <andrew@example.com>
2018-12-31 19:11:08 -0500

    # This is a combination of 5 commits. # This is the 1st commit message:
    add search by kind functionality
    
    # This is the commit message #1:
    
    test3
    
    # This is the commit message #2:
    
    add to ReadMe
    
    # This is the commit message #3:
    
    Revert "add to ReadMe"
    
    This reverts commit fee800414f10b558c123b5e54e2d4aebf702c808.
    
    # This is the commit message #4:
    
    Revert "test3"
    
    This reverts commit cfe378f2671502d6bd778237767fed303b8f1e54.

--- teafront/src/app/home/home.component.ts
+++ teafront/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Response } from '@angular/http';
 import { TeaService } from '../services/tea.service';
 import Tea from '../models/tea.model';
 import { Component, OnInit } from '@angular/core';
+import { exists } from 'fs';
+import { async } from 'q';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +15,26 @@ export class HomeComponent implements OnInit {
   constructor(private teaService: TeaService) {}
 
   teasList: Tea[];
+  kindsList: string[] = [];
+  kindSelection: string;
   ngOnInit() {
     this.teaService.getTeas()
     .subscribe(teas => {
       this.teasList = teas;
       console.log(teas);
     })
+
+    this.teaService.getTeas()
+    .subscribe(teas => {
+      for(let tea of teas) {
+        if(!this.kindsList.includes(tea.kind)){
+          this.kindsList.push(tea.kind);
+        }
+      }
+      console.log(this.kindsList);
+    })
+  
+  
   }
 
 }

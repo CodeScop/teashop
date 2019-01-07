93802511381b86aa41a02be7a5213934889e4520
Andrew <andrew@example.com>
2019-01-07 16:42:14 -0500

    add front-end authentication

--- teafront/src/app/app-routing.module.ts
+++ teafront/src/app/app-routing.module.ts
@@ -5,16 +5,22 @@ import { HomeComponent } from "./home/home.component";
 import { TeaComponent } from "./tea/tea.component";
 import { ShowComponent } from "./show/show.component";
 
+import { SignInComponent } from './authentication/sign-in/sign-in.component';
+import { SignUpComponent } from './authentication/sign-up/sign-up.component';
+import { AuthGuard } from './authentication/auth.guard';
+
 const routes: Routes = [
   { path: '', redirectTo: "/home", pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'teas', component: TeaComponent },
-  { path: 'tea/:id', component: ShowComponent}
-
+  { path: 'teas', component: TeaComponent, canActivate: [AuthGuard] },
+  { path: 'tea/:id', component: ShowComponent },
+  { path: 'signin', component: SignInComponent },
+  { path: 'signup', component: SignUpComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }

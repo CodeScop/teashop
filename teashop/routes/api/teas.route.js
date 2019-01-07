aba354fd085d88a24afeb4489a205b33705b9c00
Andrew Adkins <adkinsar3@gmail.com>
2019-01-07 15:51:56 -0500

    JSON Web Token implemented

--- teashop/routes/api/teas.route.js
+++ teashop/routes/api/teas.route.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var TeaController = require('../../controllers/tea.controller.js');
-
+const isAuth = require('../../middleware/is-auth');
 router.get('/', TeaController.getTeas);
-router.post('/', TeaController.createTea);
-router.put('/', TeaController.updateTea);
-router.delete('/:id', TeaController.removeTea);
+router.post('/', isAuth, TeaController.createTea);
+router.put('/', isAuth, TeaController.updateTea);
+router.delete('/:id', isAuth, TeaController.removeTea);
 router.get('/:id', TeaController.getTea);
 
 module.exports = router;
